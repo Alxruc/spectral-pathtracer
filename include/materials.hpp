aa290a7758b17310c9f605e5a84ad2fedd007485
Alxruc <ruchtialex@gmail.com>
2026-07-02 19:40:09 +0200

    metal stuff

--- include/materials.hpp
+++ include/materials.hpp
@@ -4,9 +4,10 @@
 #include <hip/hip_runtime.h>
 
 enum class MatType { Lambertian, Metal, Dielectric };
-enum class MetalType { Chrome, Gold }; // only chrome implemented right now
+enum class MetalType { Chrome, Gold };
 
 struct MetalProps {
+    MetalType metal;
     float roughness;
 };
 
@@ -34,10 +35,10 @@ __host__ __device__ inline Material make_lambertian(float albedo) {
         };
 }
 
-__host__ __device__ inline Material make_metal(float roughness) {
+__host__ __device__ inline Material make_metal(MetalType metal, float roughness) {
     return Material{
         .type = MatType::Metal,
-        .metal = { .roughness = roughness }
+        .metal = { .metal = metal, .roughness = roughness }
     };
 }
 
@@ -63,6 +64,11 @@ __device__ float getRefractiveIndex(MetalType metal, float lamda_nm) {
             return -0.074 + 7.308 * lamda_mu - 4.678 * powf(lamda_mu, 2.0) + 0.985 * powf(lamda_mu, 3.0);
             break;
         case MetalType::Gold:
+            // because of gold's weird curve I split it into two different polynomials
+            // split at 500nm; index and coefficient by Johson and Christy 1972
+            return lamda_mu < 0.5
+                ? 2.335 + -13.68 * lamda_mu + 56.12 * powf(lamda_mu, 2.0) - 67.86 * powf(lamda_mu, 3.0)
+                : 2.309 - 5.697 * lamda_mu + 4.598 * powf(lamda_mu, 2.0) - 1.052 * powf(lamda_mu, 3.0);
             break;
         default:
             __builtin_unreachable();
@@ -80,6 +86,10 @@ __device__ float getExtinctionIndex(MetalType metal, float lamda_nm) {
             return -0.026 + 10.32 * lamda_mu - 9.494 * powf(lamda_mu, 2.0) + 2.871 * powf(lamda_mu, 3.0);
             break;
         case MetalType::Gold:
+            // Johnson and Christy 1974L mapped to two seperate cubic polynomials split at 500nmn
+            return lamda_mu < 0.5
+                ? -2.984 + 35.08 * lamda_mu - 81.56 * powf(lamda_mu, 2.0) + 61.45 * powf(lamda_mu, 3.0)
+                : -4.599 + 16.00 * lamda_mu - 6.453 * powf(lamda_mu, 2.0) + 1.601 * powf(lamda_mu, 3.0);
             break;
         default:
             __builtin_unreachable();
